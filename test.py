d544704f5c03992f29e0f33a16810bd7a0e06f32
3ddie-papi <epiceddiekim@gmail.com>
2026-06-26 16:28:36 +0300

    Able to add underscores but a flaw when a string has same uppercase letters

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
 def main():
     camelcaseinput = input("camelCase: ")
     uppercaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    indexes = []
+    uppercaselist = []
     for letter in camelcaseinput:
         for uppercase in uppercaseletters:
             if letter == uppercase: 
-                indexofuppercase = camelcaseinput.index(letter)
-                indexes.append(indexofuppercase)
-    print(addunderscore(indexes, camelcaseinput))
+                
+                uppercaselist.append(letter)
+    print(addunderscore(uppercaselist, camelcaseinput))
 
        
                 
-def addunderscore(nmbr, originalinput):
-    originalinput = list(originalinput)
-    for i in nmbr:
-        originalinput.insert(i, "_")        
-        print(i)
-    snakecase = "".join(originalinput)
-    print(snakecase)
-
+def addunderscore(letterlist, originalinput):
+    for i in letterlist:
+        if i in originalinput:
+            originalinput = originalinput.replace(i, f"_{i}")
+    return originalinput.lower()
 
 
     
