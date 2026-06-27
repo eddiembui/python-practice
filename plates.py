6c3b16a3aa30812f6c673f94ed6705f10aff6949
3ddie-papi <epiceddiekim@gmail.com>
2026-06-27 23:57:05 +0300

    Corrected on the conditionals

--- plates.py
+++ plates.py
@@ -10,30 +10,28 @@ def main():
 def is_valid(s):
   y = True
 
-  # checks if the first 2 letters are alphabets, minimum characters are 2 and maximum characters are 6, a space is not in the str
-  if s[0:2].isalpha() and 2 <= len(s) <= 6 and " " not in s :
-    y = True
-  else:
-    y = False
-
   # loops through the punctuation marks and checks if any of them are in the str
   for i in string.punctuation:
     if i in s:
       y = False
-  firstnumberindex = 0
+  firstnumberindex = None
 
   # this loop gets the index of the first number in the str
   for j in s:
     if j.isnumeric():
       firstnumberindex = s.index(j)
       break
-  
-  # checks if the str ends in numeric from the initial numeric and also if that first number is not a 0
-  if s[firstnumberindex:].isnumeric() and s[firstnumberindex] != "0":
+  """
+    checks if the str ends in numeric from the initial numeric and also if that first number is not a 0 and if the first 2 two characters are alphabets
+    and check if the length is a minimum of 2 and a maximum of 6 and there's no space in the str
+  """
+  if s[firstnumberindex:].isnumeric() and s[firstnumberindex] != "0" and s[0:2].isalpha():
+    y = True
+  elif firstnumberindex == None and 2 <= len(s) <= 6 and " " not in s:
     y = True
   else:
     y = False  
-
+  
 
   return y
 
