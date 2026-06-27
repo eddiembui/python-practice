89d90d52d2c6d7739c54af155a20ec97e5f4390d
3ddie-papi <epiceddiekim@gmail.com>
2026-06-27 18:50:49 +0300

    Fixed the duplicated issue

--- test.py
+++ test.py
@@ -1,22 +1,14 @@
 def main():
     camelcaseinput = input("camelCase: ")
-    uppercaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    uppercaselist = []
-    for letter in camelcaseinput:
-        for uppercase in uppercaseletters:
-            if letter == uppercase: 
-                
-                uppercaselist.append(letter)
-    print(addunderscore(uppercaselist, camelcaseinput))
-
-       
-                
-def addunderscore(letterlist, originalinput):
-    for i in letterlist:
-        if i in originalinput:
-            originalinput = originalinput.replace(i, f"_{i}")
-    return originalinput.lower()
-
-
+    snake_case = ""
     
+    for letter in camelcaseinput:
+        # If the letter is uppercase, add an underscore before it
+        if letter.isupper():
+            snake_case += "_" + letter.lower()
+        else:
+            snake_case += letter
+
+    print(snake_case)
+
 main()
