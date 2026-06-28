7f90874185e35263f0a449b2f7b5572f686ed16e
3ddie-papi <epiceddiekim@gmail.com>
2026-06-28 14:34:41 +0300

    Calories counted

---
+++ nutrition.py
@@ -0,0 +1,32 @@
+def main():
+  fruits_calories = {
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Peach": 60,
+    "Pear": 100,
+    "Pineapple": 50,
+    "Plums": 70,
+    "Strawberries": 50,
+    "Sweet Cherries": 100,
+    "Tangerine": 50,
+    "Watermelon": 80 
+  }
+
+  fruit_input = input("Enter a fruit: ").title()
+  calories = fruits_calories.get(fruit_input)
+  if calories == None:
+    print(end = "")
+  else:
+    print(f"Calories: {calories}")
+
+main()
