3addf2cd8403c9a801864b1c823d57d355545589
3ddie-papi <epiceddiekim@gmail.com>
2026-06-30 01:23:42 +0300

    Removed unnecessary variables and made the code efficient

--- coke.py
+++ coke.py
@@ -1,34 +1,20 @@
-coin_inserted = 0
-soda_price = 50
 def main():
-  global soda_price
-  balance = soda_price - coin_inserted
-  check_input(balance)
-  balance = soda_price - coin_inserted
-  while balance > 0:
-    check_input(balance)
-    balance = balance - coin_inserted
-  else:
-    absolute_balance = abs(balance)
-    print(f"Change owed: {absolute_balance}")
-
+  soda_price = 50
+  balance = soda_price
   
+  while balance > 0:
+    user_coin = check_input(balance)
+    balance -= user_coin
+  else:
+    print(f"Change owed: {abs(balance)}")
+
 
 def check_input(amount_due):
-  global coin_inserted
-  global soda_price 
-  y = True
-  while y == True:
-    
+  while True:
     print(f"Amount Due: {amount_due}")
     coin_inserted = int(input("Insert a coin: "))
     if coin_inserted == 25 or coin_inserted == 10 or coin_inserted == 5 :
-      y = False
-    else:
-      y = True
-
-
-  return coin_inserted
-
+      return coin_inserted
+ 
  
 main()
