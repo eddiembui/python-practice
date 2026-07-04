763e5c91e91e723c85c6ce15c8393516c198c4bc
3ddie-papi <epiceddiekim@gmail.com>
2026-07-04 15:43:46 +0300

    I got the price from the api response and calculated the bitcoin price

---
+++ bitcoin.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+
+def main():
+  
+  try:
+    cmd_float = float(sys.argv[1])
+    try:
+      btc_response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=9d63a05d59ca96e6815d8d6a6e6062ed07db5476aa5c54727f6eb9560f3da15c")
+      json_response = btc_response.json()
+      inside_dict = json_response['data']
+      inside_dict2 = float(inside_dict['priceUsd'])
+      no_of_btcs = cmd_float * inside_dict2
+      print(f"${round(no_of_btcs, 4):,}")
+    except requests.RequestException:
+      sys.exit("API Error will be fixed")
+   
+   
+  except ValueError:
+    sys.exit("Command-line argument is not a number")
+  except IndexError:
+    sys.exit("Missing command-line argument")
+
+main()
