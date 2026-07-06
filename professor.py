41796c3d14fd960466452580629f8ef660b61ed1
3ddie-papi <epiceddiekim@gmail.com>
2026-07-06 14:12:16 +0300

    Corrected the code by returning the correct values

--- professor.py
+++ professor.py
@@ -1,11 +1,12 @@
-import random 
+import random
 
 def main():
   user_level = get_level()
   i = 0
   score = 0
   while i < 10:
-    x, y = generate_integer(user_level)
+    x = generate_integer(user_level)
+    y = generate_integer(user_level)
     wrong_attempts = 0
     while wrong_attempts != 3:
       try:
@@ -14,52 +15,49 @@ def main():
         if user_answer != sum_answer:
           print("EEE")
           wrong_attempts += 1
-          
+
         elif user_answer == sum_answer:
           score += 1
           break
       except ValueError:
         print("EEE")
         wrong_attempts += 1
-        
+
     else:
       print(f"{x} + {y} = {sum_answer}")
-      
+
     i += 1
   else:
     print(f"Score: {score}")
-  
+
 
 def get_level():
   while True:
     try:
       level_input = int(input("Level: "))
       if level_input == 1 or level_input == 2 or level_input == 3:
-        
+
         return level_input
-        
+
       else:
         print("Input is not in range")
-        
+
     except ValueError:
       print("Input is a str")
-      
-    
+
+
 
 
 def generate_integer(level):
-  
+
     if level == 1:
-      x = random.randint(0,6)
-      y = random.randint(0,6)
+      digit = random.randint(0,9)
     elif level == 2:
-      x = random.randint(7,12)
-      y = random.randint(7,12)
+      digit = random.randint(10,99)
     elif level == 3:
-      x = random.randint(13,18)
-      y = random.randint(13,18)
-    return x, y
-    
+      digit = random.randint(100,999)
+    return digit
+
 
 
 if __name__ == "__main__":
