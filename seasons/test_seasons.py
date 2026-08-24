47c641c133b12d218f628e1e5c6f0e9b28c7123d
3ddie-papi <epiceddiekim@gmail.com>
2026-08-24 19:39:11 +0300

    Correction on tests

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,8 +2,8 @@ from seasons import date_format
 import pytest
 
 def test_correct_format():
-  assert date_format("2025-08-14") == "Five hundred twenty-five thousand, six hundred minutes"
-  assert date_format("2024-08-14") == "One million, fifty-one thousand, two hundred minutes"
+  assert date_format("2025-08-24") == "Five hundred twenty-five thousand, six hundred minutes"
+  assert date_format("2024-08-24") == "One million, fifty-one thousand, two hundred minutes"
 
 def test_sysexit():
   with pytest.raises(SystemExit) as sample:
